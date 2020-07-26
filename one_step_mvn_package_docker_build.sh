29fee57999bfdcadd90b775045e0f281f307b010
jyl <kelvin94@live.cn>
2020-07-26 13:04:35 -0700

    Updated startup script

--- one_step_mvn_package_docker_build.sh
+++ one_step_mvn_package_docker_build.sh
@@ -1,5 +1,10 @@
 
 mvn clean package
+docker container rm disseckill-rabbitmq
+docker container rm disseckill-zookeeper
+docker container rm disseckill-redis
+docker container rm disseckill-order
+
 docker build -t jylkelvin/disseckill-order ./order/
 docker build -t jylkelvin/disseckill-mq ./mq/
 docker build -t jylkelvin/disseckill-cache ./cache/
