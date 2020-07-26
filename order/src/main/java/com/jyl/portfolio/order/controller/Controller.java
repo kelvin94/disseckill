5018f4400c5f21f2193718791aa3727c23b77b25
jyl <kelvin94@live.cn>
2020-07-25 18:35:59 -0700

    Enabled CORS

--- order/src/main/java/com/jyl/portfolio/order/controller/Controller.java
+++ order/src/main/java/com/jyl/portfolio/order/controller/Controller.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class Controller {
     private static Logger logger = LogManager.getLogger(Controller.class.getSimpleName());
 
